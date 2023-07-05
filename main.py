317ec5e5614843ac77a3de0d8aef82e330d439ab
Atharva Ghadi <atharva_main@Atharvas-MacBook-Pro-2.local>
2023-07-05 20:01:25 +0530

    Com 2

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import cvzone
 import math
 from sort import *
 
+# Install requirements.txt and torch vision properly
+
 model = YOLO("yoloWeights/yolov8x.pt")
 cap = cv2.VideoCapture("videos/vid.mp4")
 
@@ -14,13 +16,15 @@ className=["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train",
             ,"couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote","keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator"
             ,"book", "clock","vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
-mask=cv2.imread("images/mask.png")
+mask=cv2.imread("images/mask.png")          #Masking the unnecessary area
 
-tracker = Sort(max_age=15, min_hits=3, iou_threshold=0.3)
+tracker = Sort(max_age=15, min_hits=3, iou_threshold=0.3)           #Tracking the objects through multiple frames
+
+#Limits for the line
 limits1=[210,535,610,535 ]
 limits2=[200,600,610,600]
 
-counts=[]
+counts=[]           #List of object IDS
 
 while True:
     success,img =cap.read()
